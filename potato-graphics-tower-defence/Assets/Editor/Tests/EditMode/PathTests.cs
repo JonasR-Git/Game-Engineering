58838e5c98cd81a33820e0dbb4a73bdf08b9cb7d
JonasR <44968998+JonasR-Git@users.noreply.github.com>
2022-05-16 09:34:05 +0200

    Set up structure for the TestRunner and first framwork of the Test for the Path class

---
+++ potato-graphics-tower-defence/Assets/Editor/Tests/EditMode/PathTests.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class PathTests
+{
+    [Test]
+    public void Waypoints_Test()
+    {
+
+    }
+   
+}
--- potato-graphics-tower-defence/Assets/Scripts/Path.cs
+++ potato-graphics-tower-defence/Assets/Scripts/Path.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Waypoints : MonoBehaviour
+public class Path : MonoBehaviour
 {
     public static Transform[] waypoints;
 
     void Awake()
     {
-        waypoints = new Transform[tranform.childCount];
+        waypoints = new Transform[transform.childCount];
         for (int i = 0; i < waypoints.Length; i++)
         {
             waypoints[i] = transform.GetChild(i);
