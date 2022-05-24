ca1e56a3ff65a10fa21c0f0a0535208313c53ba1
JonasR <44968998+JonasR-Git@users.noreply.github.com>
2022-05-24 18:19:04 +0200

    first start for turret

--- potato-graphics-tower-defence/Assets/Scripts/Turret.cs
+++ potato-graphics-tower-defence/Assets/Scripts/Turret.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
+    //public for testing
+    [HideInInspector]
+    public Transform target;
+
+    public float range = 12f; //3 Tiles
+
+    public string enemyTag = "Enemy";
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,50 @@ public class Turret : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //tower needs nothing to do while he has no target
+        if (target == null)
+            return;
+    }
+
+    //drawing the range from the turret
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+
+
+    //The commented out lines are for different target priorising
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        float farthestDistance = 0;
+        GameObject nearestEnemy = null;
+        //GameObject farthestEnemy = null;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+            /*if (distanceToEnemy > farthestDistance)
+            {
+                farthestDistance = distanceToEnemy;
+                farthestEnemy = enemy;
+            }*/
+        }
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
     }
 }
+
